52434383e683c04d97045d654c7766bd8a3af158
phddone <superdriver405@gmail.com>
2016-08-29 21:33:52 -0700

    finish

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,22 +62,35 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% FORWARD PROPOGATION
+a1 = [ones(m,1) X];
+z2 = (a1*Theta1');
+a2 = [ones(size(z2,1),1) sigmoid(z2)];
+h_theta = sigmoid(a2*Theta2');
+a3 = h_theta;
+y_matrix = eye(num_labels)(y,:);
+J = (-sum(sum(y_matrix.*log(h_theta))) - sum(sum((1-y_matrix).*(log(1-h_theta)))))/m;
+
+% REGULARIZATION
+regularization_term = (lambda/(2*m))*((sum(sum(Theta1(:,2:end).^2))) + sum(sum(Theta2(:,2:end).^2)));
+J = J + regularization_term;
 
 
+% BACK PROPOGATION
+d3 = a3 - y_matrix;                                             % has same dimensions as a3
+d2 = (d3*Theta2).*[ones(size(z2,1),1) sigmoidGradient(z2)];     % has same dimensions as a2
+
+D1 = d2(:,2:end)' * a1;    % has same dimensions as Theta1
+D2 = d3' * a2;    % has same dimensions as Theta2
+
+Theta1_grad = Theta1_grad + (1/m) * D1;
+Theta2_grad = Theta2_grad + (1/m) * D2;
 
 
+% REGULARIZATION OF THE GRADIENT
 
-
-
-
-
-
-
-
-
-
-
-
+Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda/m)*(Theta1(:,2:end));
+Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + (lambda/m)*(Theta2(:,2:end));
 
 
 % -------------------------------------------------------------
